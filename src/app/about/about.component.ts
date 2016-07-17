0e7cfa65760c560e03e4fec6b1f20af5bcbd465e
jorawarsingh <jorawar@hotmail.se>
2016-07-17 11:32:09 +0200

    codelyzer tslint rules for angular2 best prectice

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,23 +1,23 @@
 /**
  * Created by js on 2016-06-30.
  */
-import { Component , OnInit, Inject} from "@angular/core";
+import { Component , OnInit , Inject} from "@angular/core";
 import { Title } from "@angular/platform-browser";
 @Component({
     template: "<h1>About page!</h1>",
 })
 export class About implements OnInit{
 
-    id: number;
-    name: string;
-    ngOnInit() {
-        console.log("App component loaded");
-        this.titleService.setTitle( "about" );
+   private id: number;
+   private name: string;
+   public ngOnInit() {
+       // console.log("App component loaded");
+        this.setTitle( "about" );
     }
 
-    public constructor(@Inject(Title) private titleService: Title ) {}
-    
-    setTitle( newTitle: string) {
+   public constructor(@Inject(Title) private titleService: Title ) {}
+
+    private  setTitle( newTitle: string) {
         this.titleService.setTitle( newTitle );
     }
 

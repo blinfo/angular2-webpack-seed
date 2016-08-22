e01904ac06ee45983d647258d64f8ebb54f17f09
jorawarsingh <jorawar@hotmail.se>
2016-08-22 23:13:14 +0200

    refactoring

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,10 +4,9 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 @Component({
-    template: "<h1>Contact page</h1>",
+    templateUrl: "contact.html",
 })
 export class ContactComponent implements OnInit {
-
    public ngOnInit() {
         // console.log("App component loaded");
         this.titleService.setTitle( "contact" );

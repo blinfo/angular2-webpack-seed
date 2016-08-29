9e46876d21fc489d841a3331f2eb1ed69669363d
jorawarsingh <jorawar@hotmail.se>
2016-08-29 22:38:33 +0530

    some refactoring

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -21,6 +21,10 @@ var helpers = require('./helpers');
                   {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
                   {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
                   {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+                  {
+                        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+                        loader: 'url-loader?limit=10000',
+                    },
                   {test: /\.html$/, loader: 'html'},
                   {test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,loader : 'file-loader'},
                   {test: /\.json$/, loader: "json-loader"},
